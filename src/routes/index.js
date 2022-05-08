777e147697ea9cc5ecf9e1594e395207d05b853c
BrenoRev <silvabreno462@gmail.com>
2022-05-07 21:48:00 -0300

    Rotas personalizadas e privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+ 
 import Login from '../pages/Login';
-import NotFound from '../pages/Page404';
-
-export default function Rotas() {
-    return (
-        <Routes>
-            <Route exact path="/" element={<Login/>} />
-            <Route path="*" element={<NotFound/>} />
-        </Routes>
-    )
+import Page404 from '../pages/Page404';
+import PrivateRoute from './PrivateRoute';
+ 
+export default function Routers() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<PrivateRoute />}>
+        <Route exact path="/" element={<Login/>} />
+      </Route>
+ 
+      <Route exact path="/login" element={<Login />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
 }
