c636f619cfa17a0bc199b3ee6d7078e291017973
BrenoRev <silvabreno462@gmail.com>
2022-05-12 11:35:31 -0300

    Base Finalizada

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import axios from '../../services/axios';
+
 import { Title } from './styled'
 import { Container } from '../../styles/GlobalStyles'
 import * as exampleActions from '../../store/modules/example/actions';
+
 export default function Login() {
-
-    const dispatch = useDispatch();
-
-    function handleClick(e)  {
-        e.preventDefault();
-        dispatch(exampleActions.clicaBotaoRequest());
-   }
-
-
-   React.useEffect(() => {
-    axios.get('/alunos').then(response => {
-        console.log(response.data);
-    });
-   }, []);
-
   
     return (
         <Container>
-            <Title>Login
-                <p>Breno</p>
-                <p>lorem</p>
-                <button onClick={handleClick} >Enviar</button>
-            </Title>
+            <h1> Login </h1>
         </Container>
     )
 }
