b18e63e20e58f6de62dde10faa5ffc438624d6d9
BrenoRev <silvabreno462@gmail.com>
2022-05-12 02:24:27 -0300

    Redux persist

--- src/store/modules/example/sagas.js
+++ src/store/modules/example/sagas.js
@@ -2,10 +2,11 @@ import { call, put, all, takeLatest } from 'redux-saga/effects'
 import { toast } from 'react-toastify';
 import * as actions from './actions'
 import * as types from '../types';
+
 const requisicao = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            resolve('ok')
+            resolve()
         }, 2000);
     })
 };
