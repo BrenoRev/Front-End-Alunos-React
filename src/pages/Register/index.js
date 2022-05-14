c452f61e47e470acbe4ee8216f4d212d1e651ba2
BrenoRev <silvabreno462@gmail.com>
2022-05-14 17:32:46 -0300

    [API] Criando usuarios

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,16 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from '../../services/axios';
 
-import { Title } from './styled'
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+import { Form } from './styled'
 import { Container } from '../../styles/GlobalStyles'
+import history from '../../services/history';
+
 import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Register() {
-  
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(name, email, password)
+
+        let formErrors = false;
+
+        if(name.length < 3 || name.length > 255) {
+            formErrors = true;
+            toast.error('O nome deve ter entre 3 e 255 caracteres');
+        }
+        if(password.length < 6 || password.length > 50) {
+            formErrors = true;
+            toast.error('A senha deve ter entre 6 e 50 caracteres');
+        }
+        if(email.length < 6 || email.length > 255) {
+            formErrors = true;
+            toast.error('O email deve ter entre 6 e 255 caracteres');
+        }
+        
+        if(formErrors)
+            return;
+        
+        axios.post('/users/', {
+            nome: name,
+            email: email,
+            password: password
+        }).then(() => {
+            toast.success('Aluno cadastrado com sucesso');
+            history.push('/');
+
+        }).catch(error => {
+            const errors = get(error, 'response.data.errors', []);
+            errors.map(error => toast.error(error));
+        }
+        )
+
+    }
+
     return (
         <Container>
-            <h1> Register </h1>
+            <h1> Crie sua conta </h1>
+
+            <Form onSubmit={handleSubmit}>
+                <label htmlFor="name">Nome:
+                    <input 
+                        type="text" 
+                        placeholder="Nome:" 
+                        value={name} 
+                        onChange={e => setName(e.target.value)}/>
+                </label>
+                
+                <label htmlFor="email">Email:
+                    <input 
+                        type="email" 
+                        placeholder="Email:" 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}/>
+                </label>
+
+                <label htmlFor="password">Senha:
+                    <input 
+                        type="password" 
+                        placeholder="Senha:" 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}/>
+                </label>
+
+                <button type="submit">Criar conta</button>
+            </Form>
         </Container>
     )
 }
--- src/pages/Register/styled.js
+++ src/pages/Register/styled.js
@@ -1,5 +1,42 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Title = styled.h1`
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+
+    label {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    input {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 10px;
+        width: 100%;
+        margin-top: 5px;
+
+        &:focus {
+            border: 1px solid ${colors.primaryColor};
+        }
+    }
     
+    button {
+        cursor: pointer;
+        background: ${colors.primaryColor};
+        border: none;
+        color: #fff;
+        padding: 10px 20px;
+        border-radius: 4px;
+        font-weight: 700;
+        transition: all 1000ms;
+        
+        &:hover {
+            filter: brightness(85%); 
+        }
+    }
+
 `
