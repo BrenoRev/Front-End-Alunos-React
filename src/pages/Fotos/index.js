b8a6f902751aed8efe86580af68d9da7ba44ea96
BrenoRev <silvabreno462@gmail.com>
2022-05-18 20:45:45 -0300

    [API] Salvando nvoas imagens

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import axios from '../../services/axios';
-
+import { Form } from './styled';
 import { Container, Title } from '../../styles/GlobalStyles'
-
+import history from '../../services/history';
+import { get } from 'lodash'; 
+import { toast } from 'react-toastify';
 
 export default function Photos() {
-  
+
+    const id = window.location.pathname.split('/')[2];
+    const [foto, setFoto] = React.useState('');
+
+    const handleChange = async (event) => {
+        const foto = event.target.files[0];
+        const fotoUrl = URL.createObjectURL(foto);
+        setFoto(fotoUrl);
+
+        const formData = new FormData();
+        formData.append('foto', foto);
+        formData.append('aluno_id', id);
+        
+        try{
+            await axios.post('/fotos', formData);
+            toast.success('Foto enviada com sucesso!');
+        } catch (err) {
+            toast.error('Erro ao enviar foto');
+        }
+    }
+
+    React.useEffect(() => {
+        const getData = async () => {
+            try {
+                const { data } = await axios.get(`/alunos/${id}`);
+                const Foto = get(data, 'Fotos[0].url', '');
+                setFoto(Foto);
+            } catch(err) {
+                toast.error('Erro ao carregar foto');
+                history.push('/')
+            }
+        }
+
+        getData();
+
+    }, []);
     return (
         <Container>
             <Title> Photos </Title>
+
+            <Form>
+                <label htmlFor="foto">
+                    {foto ? <img src={foto} alt="Foto" /> : 'Selecionar'}
+                    <input type="file" id="foto" onChange={handleChange}/>
+                </label>
+
+            </Form>
         </Container>
     )
 }
--- src/pages/Fotos/styled.js
+++ src/pages/Fotos/styled.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Title = styled.h1`
-    
+export const Form = styled.form`
+    label {
+        width: 180px;
+        height: 180px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: white;
+        border: 5px dashed ${colors.primaryColor};
+        margin: 30px auto;
+        cursor: pointer;
+        border-radius: 50%;
+        overflow: hidden;
+    }
+
+    input {
+        display: none;
+    }   
+
+    img {
+        width: 180px;
+        height: 180px;
+    }
 `
